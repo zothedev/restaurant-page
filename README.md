cff37d21db8e1ee98f69064d3ea7e59f99ae4d60
zothedev <64105313+zothedev@users.noreply.github.com>
2025-07-10 13:08:19 -0400

    Add image credit text at the bottom of home page

--- README.md
+++ README.md
@@ -1,7 +1,16 @@
-# restaurant-page
+# Restaurant Page
+
+This project was my first experience building a website using Webpack (and npm packages).
+While the new project layout was daunting at first, once I learned the benefits of bundlers
+and npm packages it became clear why we need all these files that are produced after running
+`npm init -y`. Using plugins and loaders for our non-javascript files (such as HTML, CSS, and local image paths) was also quite a change. 
+
+This project was also a great opportunity to practice my DOM manipulation. This project only
+includes Javascript code. All the HTML elements and CSS styles are added via the DOM. The tabs
+for the website simply remove the content of the current page and displays the content of the
+requested page, instead of the tab switching the user to a different HTML file.
 
 
 
 ## Image Credit
-- Pizza
-https://images.unsplash.com/photo-1566843972142-a7fcb70de55a?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D
+- [Pizza Image](https://images.unsplash.com/photo-1604382354936-07c5d9983bd3?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D) by [Shourav Sheikh](https://unsplash.com/@shouravsheikh)
--- src/home.js
+++ src/home.js
@@ -10,14 +10,14 @@ export function createHomepage() {
 
     // create and append the image
     const image = document.createElement('img');
-    image.src = "https://images.unsplash.com/photo-1566843972142-a7fcb70de55a?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+    image.src = "https://images.unsplash.com/photo-1604382354936-07c5d9983bd3?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
     image.style.width = '305px'; // style
     image.style.marginLeft = '8%';
     content.appendChild(image);
 
     // create and append the comments
-    createComment("The best pizza I've ever had in my life!!!",'-Someone, probably.')
-    createComment("Mario's is the best restaurant on this street!!!","(Mario's is the only restaurant on this street)");
+    createComment("The best pizza I've ever had in my life!!!", '-Someone, probably.')
+    createComment("Mario's is the best restaurant on this street!!!", "(Mario's is the only restaurant on this street)");
 
     // apply other styles
 
@@ -28,48 +28,60 @@ export function createHomepage() {
     });
 
     // body styles
-    const bodyElement = document.querySelector('body'); 
+    const bodyElement = document.querySelector('body');
     bodyElement.style.backgroundColor = 'rgb(77,108,186)';
     bodyElement.style.display = 'flex';
     bodyElement.style.flexDirection = 'column';
     bodyElement.style.alignItems = 'center';
+
+    // create footer
+    let imageCredit = document.createElement('p');
+    let imageCreditLink = document.createElement('a');
+
+    content.appendChild(imageCredit);
+
+    imageCredit.textContent = 'Pizza Image by ';
+    imageCreditLink.textContent = 'Shourav Sheikh on unsplash';
+    imageCreditLink.href = 'https://unsplash.com/photos/pizza-with-cheese-and-green-leaf-a66sGfOnnqQ';
+
+    imageCredit.appendChild(imageCreditLink);
 }
 
 export function createComment(quoteText, authorText) {
 
-        // create, label, and append the comment container
-        let comment = document.createElement(`div`);
-        comment.classList.add('comment');
+    // create, label, and append the comment container
+    let comment = document.createElement(`div`);
+    comment.classList.add('comment');
 
-        // comment container styles
-        comment.style.border = 'rgba(0,0,0,50%) solid 5px';
-        comment.style.backgroundColor = 'rgb(91, 110, 160)'
-        comment.style.display = 'flex';
-        comment.style.flexDirection = 'column';
-        comment.style.alignItems = 'center';
-        comment.style.margin = '10px 0';
-        comment.style.padding = '5px';
+    // comment container styles
+    comment.style.border = 'rgba(0,0,0,50%) solid 5px';
+    comment.style.backgroundColor = 'rgb(91, 110, 160)'
+    comment.style.display = 'flex';
+    comment.style.flexDirection = 'column';
+    comment.style.alignItems = 'center';
+    comment.style.margin = '10px 0';
+    comment.style.padding = '5px';
 
-        content.appendChild(comment);
+    content.appendChild(comment);
 
-        // create, modify, and append the comment contents
+    // create, modify, and append the comment contents
 
-        // comment quote
-        let quote = document.createElement('h3')
-        quote.textContent = quoteText;
-        comment.appendChild(quote);
+    // comment quote
+    let quote = document.createElement('h3')
+    quote.textContent = quoteText;
+    comment.appendChild(quote);
 
-        // comment author
-        let author = document.createElement('p');
-        author.textContent = authorText;
+    // comment author
+    let author = document.createElement('p');
+    author.textContent = authorText;
 
-        // comment author styles
-        author.style.alignSelf = 'flex-end';
-        author.style.paddingRight = '10px';
+    // comment author styles
+    author.style.alignSelf = 'flex-end';
+    author.style.paddingRight = '10px';
 
-        comment.appendChild(author);
+    comment.appendChild(author);
 
 
-    }
+}
 
 
